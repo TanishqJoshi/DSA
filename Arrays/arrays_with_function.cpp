c0bf0f6e3a761337c1092bfdd47ee53d62187498
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-06 22:58:03 +0530

    .

--- Arrays/arrays_with_function.cpp
+++ Arrays/arrays_with_function.cpp
@@ -1,18 +1,15 @@
 #include<iostream>
 using namespace std;
-void printArray(int arr[], int size)
-{
-    cout<<"Printing the array"<<endl;
-    for(int i=0; i<size; i++)
-        {
-            cout<<arr[i]<<" ";  
-        }
+
+int printArray(int arr[], int size){
+    cout<<"Starting";
+    for(int i =0; i<size; i++){
+        cout<<arr[i];
+    }
+    cout<<"done";
 }
-int main()
-{
-    int first[4] = {11,12,13,14};
-    printArray(first, 4);
+int main(){
+    int third[15]={2,7};
+    printArray(third,15);
     return 0;
-
 }
-
