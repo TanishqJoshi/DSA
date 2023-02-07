fd09ba7f695b30129bda36e080990db7fa1916f3
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-07 23:58:50 +0530

    Printing sum of elements of array

---
+++ Arrays/lec9_PrintingSumofArray_Arrays.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int printSum(int arr[], int n){
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum = sum+arr[i];
+    }
+    return sum;
+}
+int main(){
+    int size;
+    cin>>size;
+    int num[100];
+    for(int i=0; i<size; i++){
+        cin>>num[i];
+    }
+    cout<<"The sum of all elements of array is "<<printSum(num, size)<<endl;
+    return 0;
+}
