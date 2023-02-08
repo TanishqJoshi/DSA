94abedec13672776fdc402573473642f230ece0a
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-08 23:25:15 +0530

    Linear Search in Array

--- Arrays/lec9_LinearSearch_Arrays.cpp
+++ Arrays/lec9_LinearSearch_Arrays.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 bool search(int arr[], int size, int key){
-
+    for(int i=0; i<size; i++){
+        if(arr[i] == key){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
 }
 int main(){
     int arr[10] = {5,7-2,10,22,-2,0,5,22,1};
