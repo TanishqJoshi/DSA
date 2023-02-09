f2d54ab527b86059ced3188393e2f2cf416e0dbc
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-09 16:00:00 +0530

    Linear Search

--- Arrays/lec9_LinearSearch_Arrays.cpp
+++ Arrays/lec9_LinearSearch_Arrays.cpp
@@ -1,27 +1,24 @@
 #include<iostream>
 using namespace std;
-
 bool search(int arr[], int size, int key){
     for(int i=0; i<size; i++){
-        if(arr[i] == key){
+        if(key == arr[i]){
             return 1;
         }
-        else{
-            return 0;
-        }
     }
+    return 0;
 }
 int main(){
-    int arr[10] = {5,7-2,10,22,-2,0,5,22,1};
-    cout<<"enter the element to search for\n";
+    int arr[5] = {1,2,3,4,5};
+    cout<<"Enter the element you want to search ";
     int key;
     cin>>key;
-    bool found = search(arr,10,key);
+    bool found = search(arr,5,key);
     if(found){
-        cout<<"key is present";
+        cout<<"Key is Present\n";
     }
     else{
-        cout<<"Key is absent";
+        cout<<"Not found\n";
     }
     return 0;
 }
