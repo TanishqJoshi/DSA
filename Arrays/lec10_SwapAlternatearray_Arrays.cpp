fcd1d5fedc3e9c5217b09ed7a0b3d2d2f200f526
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-09 20:29:31 +0530

    Lec 10, Question 01, Swap Alternate

--- Arrays/lec10_SwapAlternatearray_Arrays.cpp
+++ Arrays/lec10_SwapAlternatearray_Arrays.cpp
@@ -4,6 +4,7 @@ void printArray(int arr[], int size){
     for(int i =0; i<size; i++){
         cout<<arr[i]<<" ";
 }
+cout<<endl;
 }
 void swapAlternate(int arr[], int size){
     for(int i=0; i<size; i+=2){
@@ -14,7 +15,10 @@ void swapAlternate(int arr[], int size){
 }
 int main(){
     int arr[6]={1,2,3,4,5,6};
+    int brr[5]={1,2,3,4,5};
     swapAlternate(arr,6);
+    swapAlternate(brr,5);
     printArray(arr,6);
+    printArray(brr,5);
     return 0;
 }
