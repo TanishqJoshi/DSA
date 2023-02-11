d05de2652d6d7b5d08fbd43ed2a3ddae39033d3b
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-11 23:50:41 +0530

    Binary search

--- Arrays/Lecture12_Arrays/BinarySearchImplement_Arrays.cpp
+++ Arrays/Lecture12_Arrays/BinarySearchImplement_Arrays.cpp
@@ -4,8 +4,8 @@ int binarySearch(int arr[], int size, int key){
     int start =0;
     int end = size-1;
 
-    int mid = (start+end)/2;
-
+    //int mid = (start+end)/2;
+    int mid = start+(end-start)/2;
     while(start<=end){
         if(arr[mid]==key){
             return mid;
@@ -16,15 +16,16 @@ int binarySearch(int arr[], int size, int key){
         else{
             end = mid-1;
         }
-        mid = (start+end)/2;
+        //mid = (start+end)/2;
+        mid = start+(end-start)/2;
     }
     return -1;
 }
 int main(){
-    int odd[5]={1,5,9,12,15};
     int even[6]={2,6,8,10,15,20};
-    int evenIndex = binarySearch(even,6,8);
-    int oddIndex = binarySearch(odd,5,20);
+    int odd[5]={1,5,9,12,15};
+    int evenIndex = binarySearch(even,6,20);
+    int oddIndex = binarySearch(odd,5,15);
     cout<<evenIndex<<endl;
     cout<<oddIndex<<endl;
     return 0;
