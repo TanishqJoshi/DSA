29a547c16c4f109193d0696f944a5b74771a24eb
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-13 00:49:51 +0530

    Question 03 BS

--- Arrays/Lecture13_BinarySearchQuestions/Question03_BS.cpp
+++ Arrays/Lecture13_BinarySearchQuestions/Question03_BS.cpp
@@ -8,15 +8,12 @@ int target(int arr[], int size){
         while(start<=end){
             if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
                 return mid;
-                //cout<<mid;
             }
             else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
                 start = mid;
-                //cout<<mid;
             }
             else if(arr[mid]<arr[mid-1] && arr[mid]>arr[mid+1]){
             end = mid-1;
-            //cout<<mid;
             }
             mid = start + (end-start)/2;
         }
