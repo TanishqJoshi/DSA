26ff2bd6954eb3d0a3f503a4c954a7ab62e26677
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-13 00:34:59 +0530

    Question 03

--- Arrays/Lecture13_BinarySearchQuestions/Question03_BS.cpp
+++ Arrays/Lecture13_BinarySearchQuestions/Question03_BS.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 using namespace std;
 //852. Peak Index in a Mountain Array (LeetCode)
+int target(int arr[], int size){
+    int start = 0;
+    int end = size-1;
+    int mid = start + (end-start)/2;
+        while(start<=end){
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+                //cout<<mid;
+            }
+            else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
+                start = mid;
+                //cout<<mid;
+            }
+            else if(arr[mid]<arr[mid-1] && arr[mid]>arr[mid+1]){
+            end = mid-1;
+            //cout<<mid;
+            }
+            mid = start + (end-start)/2;
+        }
+        return mid;
+}
 int main(){
-    
+    int arr[7] = {0,1,2,10,5,3,2};
+    cout<<target(arr,7);
     return 0;
 }
