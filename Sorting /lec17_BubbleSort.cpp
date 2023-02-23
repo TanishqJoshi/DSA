4bbb39166c3f0cffcbdf331a740d213490ad8f29
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-02-23 23:53:47 +0530

    Bubble Sort

---
+++ Sorting /lec17_BubbleSort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0; j<n-i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+int main(){
+    int arr[5]={5,4,3,2,1};
+    bubbleSort(arr,5);
+    return 0;
+}
