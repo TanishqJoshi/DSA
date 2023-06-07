ef1b826c2ca702634df9a42a3918e400f5a3e7a8
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-06-08 01:01:46 +0530

    N to 1

--- Recursion_Striver/Re.2.cpp
+++ Recursion_Striver/Re.2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-
-void solve(string &name,int count)
+//Print your name 5 times
+/*void solve(string &name,int count)
 {
     if(count==6) return ;
     cout<<name<<" "<<count<<endl;
@@ -12,5 +12,35 @@ int main(){
     cin>>name;
     int count = 1;
     solve(name,count);
+    return 0;
+} */
+
+//Print lineraly till n
+/*void print(int n, int i){
+    if(i>n) return ;
+    cout<<i<<endl;
+    print(n,i+1);
+}
+int main(){
+    int n;
+    cin>>n;
+    int i;
+    print(n,i);
+    return 0;
+}
+*/
+
+
+
+//print N to 1
+void print(int n){
+    if(n==0) return ;
+    cout<<n<<endl;
+    print(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
     return 0;
 }
