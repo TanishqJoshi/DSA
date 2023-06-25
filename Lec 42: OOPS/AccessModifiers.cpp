52918d2b16b944c82d9804436776d4f45c1bb678
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-06-25 21:56:28 +0530

    Lec 42

--- Lec 42: OOPS/AccessModifiers.cpp
+++ Lec 42: OOPS/AccessModifiers.cpp
@@ -1,18 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-class Hero{
-    //properties
-    public:
-    int health;
-    char level;
-};
-int main(){  
-    //creating object h1 of type Hero
-    Hero ramesh;
-    ramesh.health = 70;
-    ramesh.level = 'A';
-    cout<<"Health is: "<<ramesh.health<<endl;
-    cout<<"Health is: "<<ramesh.level<<endl;
-    return 0;
+int main() {
+
+     
+int n;
+     cin>>n;
+     vector<string> arr(n);
+     for(int i = 0; i < n; i++)
+        cin>>arr[i];
+        
+        unordered_map<string, int> mp;
+        string ans = "";
+        for(int i = arr.size()-1; i >= 0; i--)
+        {
+            string str = arr[i];
+            if(mp.find(str) != mp.end())
+            continue;
+            else
+            {
+                ans += str[str.size()-2];
+                ans += str[str.size()-1];
+                mp[str]++;
+            }
+        }
+        cout<<ans;
+
+ return 0;
 }
