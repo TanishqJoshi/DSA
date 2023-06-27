df63e3622b55a038ebd777c9efcc469ff8426dc5
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-06-27 07:39:18 +0530

    Inheritance

---
+++ Lec 43: OOPS_P2/Inheritance.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Human{
+    public:
+        int height;
+        int weight;
+        int age;
+
+    public:
+        int getAge(){
+            return this->age;
+        }
+        int setWeight(int w){
+            this->weight = w;
+        }
+};
+class Male: public Human{
+    public:
+    string color;
+    void sleep(){
+        cout<<"Male Sleeping"<<endl;
+    }
+}; 
+int main(){
+    Male object1;
+    cout<< object1.age <<endl;
+    return 0;
+}
