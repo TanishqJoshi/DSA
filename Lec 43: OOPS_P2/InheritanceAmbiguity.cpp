302e8f2b75735ed9e9cbb1be69be4d91127b14ad
TanishqJoshi <tanishqjoshi2408@gmail.com>
2023-06-27 11:51:07 +0530

    Inheritance Ambiguity

---
+++ Lec 43: OOPS_P2/InheritanceAmbiguity.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+        void func(){
+            cout<<"Inside A"<<endl;
+        }
+};
+class B{
+    public:
+        void func(){
+            cout<<"Inside B"<<endl;
+        }
+};
+class C: public A, public B{
+
+};
+int main(){
+    C obj;
+    obj.A:: func();
+    obj.B:: func();
+    return 0;
+}
